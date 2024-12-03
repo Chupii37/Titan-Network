d1a84617e77aab44098e0f7618634473397db1aa
Chupii37 <162031546+Chupii37@users.noreply.github.com>
2024-12-03 12:57:25 +0700

    titan-edge.sh

--- titan-edge.sh
+++ titan-edge.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Menarik image Docker
+# Pull Docker Image
+echo "Pulling Docker image nezha123/titan-edge..."
 docker pull nezha123/titan-edge
 
 # Membuat direktori ~/.titanedge jika belum ada
+echo "Creating directory ~/.titanedge if it doesn't exist..."
 mkdir -p ~/.titanedge
 
-# Menjalankan kontainer dengan konfigurasi yang dimodifikasi (menghapus flag --network=host)
-docker run -d -v ~/.titanedge:/root/.titanedge nezha123/titan-edge
+# Menjalankan Docker container dengan --network=host dan volume mount
+echo "Running Docker container..."
+docker run --network=host -d -v ~/.titanedge:/root/.titanedge nezha123/titan-edge
+
+echo "Titan Edge container is now running."
+
